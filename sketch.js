636748dd43a31bf3ee42cf8fc85e5cd631bda5fc
mnecibi <mehdi.necibi@capgemini.com>
2022-03-06 22:59:10 +0100

    Added hide text area after button click using a cookie

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
+import Cookie from "js-cookie";
+
 let textarea = document.querySelector('.mdj__textarea');
 let yourDefinition = document.querySelector('.mdj__your-definition');
 let yourDefinitionContainer = document.querySelector('.mdj__your-definition-wrapper');
 let button = document.querySelector('.mdj__button');
 let answer = document.querySelector('.mdj__answer');
 let question = document.querySelector('.mdj__question');
+
+if(Cookie.get("played") === "true") {
+    answer.classList.remove("hide");
+    question.classList.add("hide");
+}
 
 textarea.addEventListener('input', () => {
     button.innerHTML = "Valider";
@@ -14,7 +21,7 @@ textarea.addEventListener('input', () => {
 button.addEventListener('click', () => {
     answer.classList.remove("hide");
     question.classList.add("hide");
-    document.cookie = "played=true";
+    Cookie.set("played", true);
     
     if(textarea.value) {
         yourDefinitionContainer.classList.remove("hide");
